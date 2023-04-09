952a90cb64fe7b00885407fb5d1acc7effe8d6a6
vladdemo1 <v.demchenko@student.csn.khai.edu>
2023-04-09 14:00:00 +0300

    Uploaded test package

---
+++ src/image_recognition_demo_test/main.py
@@ -0,0 +1,13 @@
+"""
+This module contains simple function for test
+"""
+
+def simple_function() -> None:
+    """
+    Simple func
+    """
+    print("-- ПерСиК 2023 --")
+    return None
+
+
+simple_function()

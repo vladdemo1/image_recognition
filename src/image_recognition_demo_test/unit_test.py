ad2f5f15c8367ea5345fe95732b89e0c0b868f8f
vladdemo1 <vladdemo140@gmail.com>
2023-11-25 12:23:04 +0200

    [ADD] added new tests

--- src/image_recognition_demo_test/unit_test.py
+++ src/image_recognition_demo_test/unit_test.py
@@ -1,3 +1,7 @@
+"""
+Unit tests for current package
+"""
+
 import unittest
 from unittest.mock import MagicMock
 
@@ -7,14 +11,26 @@ from facade import ImageFacade
 
 
 class TestImage(unittest.TestCase):
+    """
+    Main class of unittest for Image class
+    """
 
     def setUp(self):
+        """
+        default setUp config for custom tests
+        """
         pass
 
     def tearDown(self):
+        """
+        default tearDown config for custom tests
+        """
         pass
 
     def test_image_instance_is_singleton(self):
+        """
+        Main check for singleton in Image creating
+        """
         image_instance1 = Image()
         image_instance2 = Image()
 
@@ -22,14 +38,26 @@ class TestImage(unittest.TestCase):
 
 
 class TestRecognition(unittest.TestCase):
+    """
+    Main class of unittest about Recognition class
+    """
 
     def setUp(self):
+        """
+        default setUp config for custom tests
+        """
         pass
 
     def tearDown(self):
+        """
+        default tearDown config for custom tests
+        """
         pass
 
     def test_recognition_process_text_extraction(self):
+        """
+        Unit tests fot recognition and facade checks about text extraction
+        """
         mock_image_facade = MagicMock(spec=ImageFacade)
         recognition = Recognition('src/image_recognition_demo_test/edit.jpg')
         recognition.image_facade = mock_image_facade
